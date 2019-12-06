25d11f872b5355ba37f0edfb794edb6ddc6d08f3
matheusfgomes <matheusfgomes935@gmail.com>
2019-12-06 11:05:54 -0300

    2 files modified, 1 deleted

--- src/app/produto.service.ts
+++ src/app/produto.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 
 import {Observable, of} from 'rxjs';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 import { Produto } from './produto';
-import { PRODUTOS } from './mock-produtos';
+import { map } from 'rxjs/operators';
+
 
 
 @Injectable({ providedIn: 'root'})
@@ -35,48 +37,5 @@ export class ProdutoService {
 
 
 
-/*import { Injectable } from '@angular/core';
-import { Produto } from './produto';
-import { Produto } from './mock-produtos';
-import { Observable, of } from 'rxjs';
-import { MensagemServie} from './mensagem.service';
-import { AngularFireDatabase } from '@angular/fire/database';
-import { Produto } from './produto';
-import { map } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class ProdutoService {
-
-  constructor(private db: AngularFireDatabase) { }
-insert(produto: Produto) {
-console.log(produto);
-this.db.list('produtos').push(produtos)
-.then((result: any) => {
-console.log(result.key)
-});
-    
-  //    getAll(){
-  //   return this.db.list('produtos')
-  //    .snapshotChanges()
-  //  .pipe(
-  //         map(changes => {
-  //  return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
-  // })
-  // );
-  //   }
-    
-}
-  getProdutos(): Produto[]{
-    return  PRODUTOS;
-  }
-
-  // getProdutos(): Observable<Produto[]>{
-
-  //     this.mensagemService.add('ProdutoService: produto selecionado');
-  //     return of (PRODUTOS);
-  // }
-  
-  */
 
